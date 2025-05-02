d8049cc8e69ff2f1a0c4a05223b6bb42eaef2bff
Stefan Kraus <dev@stefankraus.org>
2025-05-02 09:37:55 +0200

    feat: add taiga as ticket source
    This change adds Taiga (https://taiga.io/), an open-source project
    management tool, as another source for open tickets.
    
    Closes #26
    
    Signed-off-by: Stefan Kraus <dev@stefankraus.org>

---
+++ src/tibian/sources/taiga.py
@@ -0,0 +1,63 @@
+from typing import List, Any, Literal, Union
+
+import dateutil.parser
+from pydantic import BaseModel, HttpUrl, SecretStr
+import requests
+
+from tibian.sources.ticketsource import TicketSource
+from tibian.tickets import Ticket
+
+TAIGA_API_PATH = "/api/v1"
+
+
+class TaigaAuth(BaseModel):
+    token: SecretStr
+
+
+class TaigaSource(TicketSource):
+    type: Literal["taiga"] = "taiga"
+    url: HttpUrl
+    project: int
+    auth: TaigaAuth
+
+    def get_open_tickets(self) -> List[Ticket]:
+        open_issues = []
+        search_url = self._get_taiga_search_url()
+        content = self._request_issues(search_url)
+
+        for story in content:
+            ticket = self._extract_ticket_from_story(story)
+            open_issues.append(ticket)
+
+        return open_issues
+
+    def _get_taiga_search_url(self) -> str:
+        return f"{self.url}{TAIGA_API_PATH.strip('/')}/userstories"
+
+    def _request_issues(self, search_url: str) -> list[dict[str, Any]]:
+        query_params: dict[str, Union[float, str]] = {
+            "project": self.project,
+            "status__is_closed": "false",
+        }
+        headers = {
+            "Authorization": f"Bearer {self.auth.token.get_secret_value()}",
+            "x-disable-pagination": "True",
+        }
+        response = requests.get(
+            search_url,
+            params=query_params,
+            headers=headers,
+            timeout=5,
+        )
+        response.raise_for_status()
+        content: list[dict[str, Any]] = response.json()
+        return content
+
+    def _extract_ticket_from_story(self, story: dict[str, Any]) -> Ticket:
+        key = str(story["ref"])
+        title = story["subject"]
+        status = story["status_extra_info"]["name"]
+        created = dateutil.parser.parse(story["created_date"]).date()
+
+        ticket = Ticket(key, title, status, created)
+        return ticket
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,7 @@ import pytest
 
 import tibian.config as tc
 from tibian.sources.jira import JiraSource
+from tibian.sources.taiga import TaigaSource
 from tibian.targets.stdout import StdoutTarget
 from tibian.targets.teams import TeamsTarget
 
@@ -17,8 +18,9 @@ def test_load_example_config(_example_config_file):
     config = tc.load_config(_example_config_file)
 
     assert config.sources is not None
-    assert len(config.sources) == 1
+    assert len(config.sources) == 2
     assert isinstance(config.sources[0], JiraSource)
+    assert isinstance(config.sources[1], TaigaSource)
 
     assert config.destinations is not None
     assert len(config.destinations) == 2
